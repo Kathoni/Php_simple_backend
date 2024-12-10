8c685c83801a4dc270b4c86e87e52732def91879
Kathoni <thrusherkeys@gmail.com>
2024-12-10 11:49:03 +0300

    styling my login interface

--- home.php
+++ home.php
@@ -6,18 +6,21 @@
         <link rel="stylesheet" href="style.css">
     </head>
     <body>
-        <div class="login">
+    <div class="login">
             <h1>Login</h1>
-        <div class="form"   id="form">
-        <form action="post">
-         <label>Username: </label>
-         <input type="text" id="user" name="user"><br><br>
-         <label >Password: </label>
-         <input type="password" id="pass" name="pass">
-         <input type="submit" id="btn" value="Login" name="submit" style="display: flexbox; border-radius:15px ; background-color: brown;width: 80%;margin: 15px;height: 5vb; cursor: pointer;" >
-         <h6>Don't have an account sign up <input type="submit" id="btn" value="Sign up" name="submit" style="border-radius:15px; cursor: pointer;" ></h6>
-         </form>
+            <div class="form" id="form">
+                <form action="post">
+                    <label>Username:</label>
+                    <input type="text" id="user" name="user"><br>
+                    <label>Password:</label>
+                    <input type="password" id="pass" name="pass">
+                    <input type="submit" id="btn" value="Login" name="submit">
+                    <h6>
+                        Don't have an account? Sign up 
+                        <input type="submit" id="btn" value="Sign up" name="signup">
+                    </h6>
+                </form>
+            </div>
         </div>
-    </div>
     </body>
 </html>
